30c517fd40df143abe649c2b780d98bca086b54b
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-11-05 20:13:06 +0100

    first commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,32 @@
+<script>
+  let count1 = 0;
+  let count2 = 0;
+
+  function getTotal(x,y){
+    return x + y;
+
+  }
+
+  function increment1 () {
+     count1++;
+  }
+  function increment2 () {
+     count2++;
+  }
+
+  $: string =  `total count ${getTotal(count1, count2)}`;
+  $: if(count2 > 5){
+ console.log(count2)
+  }
+</script>
+
+<h1>{string}</h1>
+
+<button on:click={increment1}>Click {count1}</button>
+<button on:click={increment2}>Click {count2}</button>
+
+<style>
+  button {
+    padding: 15px;
+  }
+</style>
